dbe98253e3bf6e723e147a4e381ee9de6a377702
Aftab69 <aftabhssn3@gmail.com>
2021-09-29 14:51:12 +0530

    last

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
     return (
         <div>
-            <p>This is Contacts page</p>
+            <p>This is Contacts page to be commited</p>
         </div>
     )
 }
