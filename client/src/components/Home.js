02ccb4de442747b12648e84e40a2c330deb3efb1
Aftab69 <aftabhssn3@gmail.com>
2021-09-29 16:51:29 +0530

    bacgroundimageadded

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div>
-            <p>Home page</p>
+        <>
+        <div className="home_background">
         </div>
+        </>
     )
 }
 
