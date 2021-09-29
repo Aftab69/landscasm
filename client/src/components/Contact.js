8765914fdf98567ae06b4c12d29f5e36656dac45
Aftab69 <aftabhssn3@gmail.com>
2021-09-29 14:42:36 +0530

    s

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
     return (
         <div>
-            <p>This is Contact page</p>
+            <p>This is Contacts page</p>
         </div>
     )
 }
