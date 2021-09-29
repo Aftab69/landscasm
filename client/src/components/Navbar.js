9b95b6cfe715c18b232636d25e0af30cc4a07613
Aftab69 <aftabhssn3@gmail.com>
2021-09-29 16:11:57 +0530

    addinglogo

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,13 +2,14 @@ import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import { NavLink } from 'react-router-dom';
+import logo from "../images/logo.png";
 
 const Navbar = () => {
     return (
         <>
                         <nav className ="navbar navbar-expand-lg navbar-light bg-light">
             <div className ="container-fluid">
-                <NavLink className ="navbar-brand" to="/">Navbar</NavLink>
+                <NavLink className ="navbar-brand" to="/"><img className="logo_size" src={logo} alt="Logo" /></NavLink>
                 <button className ="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className ="navbar-toggler-icon"></span>
                 </button>
