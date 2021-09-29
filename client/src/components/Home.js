23ff8e5939e984975b1fea4c594b9749521320a6
Aftab69 <aftabhssn3@gmail.com>
2021-09-29 17:59:10 +0530

    banneradded

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import image1 from "../images/homepage_banner.png"
 
 const Home = () => {
     return (
         <>
         <div className="home_background">
+            <div className="homepage_container">
+               <img src={image1} alt="Homepage banner" />
+            </div>
         </div>
         </>
     )
