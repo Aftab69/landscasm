011224c0dd9df62fc01117cf635651d2d883934a
Aftab69 <aftabhssn3@gmail.com>
2021-09-29 15:02:55 +0530

    Revert "part2"
    This reverts commit 6e97d63d3baee3fd799ad755b99e5cc53f0ab23c.

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
     return (
         <div>
-            <p>This is Contact page part 2</p>
+            <p>This is Contact page</p>
         </div>
     )
 }
