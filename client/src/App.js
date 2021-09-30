868aaea46c07d760850ae12927bd7ab135b5e2f7
Aftab69 <aftabhssn3@gmail.com>
2021-09-30 22:35:10 +0530

    switch

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Contact from './components/Contact';
@@ -12,11 +12,13 @@ const App = () => {
   return (
     <>
       <Navbar />
+      <Switch>
       <Route exact path="/"><Home /></Route>
       <Route path="/contact"><Contact /></Route>
       <Route path="/about"><About /></Route>
       <Route path="/signin"><Signin /></Route>
       <Route path="/signup"><Signup /></Route>
+      </Switch>
     </>
   )
 }
